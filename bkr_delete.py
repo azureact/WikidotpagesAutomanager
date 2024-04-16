643dffa0f70d7628f5daa4bd06a9160dd1c9833d
azureact <163717172+azureact@users.noreply.github.com>
2024-04-16 21:35:50 +0800

    更新 bkr_delete.py

--- bkr_delete.py
+++ bkr_delete.py
@@ -404,10 +404,11 @@ def check_pending_pages():
             pending_deleted_pages_info[page_id][0] <= -10 and score > -10 and original
         ):  # 24h->72h
             pending_deleted_pages_info[page_id][0] = score
+            record_timestamp=pending_deleted_pages_info[page_id][1]
             edit_post(
                 pending_deleted_pages_info[page_id][2],
                 pending_deleted_pages_info[page_id][3],
-                normal_delete(score, pending_deleted_pages_info[page_id][1]),
+                normal_delete(score, record_timestamp),
                 discuss,
             )
         elif (
@@ -416,10 +417,11 @@ def check_pending_pages():
             and pending_deleted_pages_info[page_id][1] - announce_time > 86400
         ):  # 72h->24h
             pending_deleted_pages_info[page_id][0] = score
+            record_timestamp=announce_time + 86400 
             edit_post(
                 pending_deleted_pages_info[page_id][2],
                 pending_deleted_pages_info[page_id][3],
-                normal_delete(score, announce_time + 86400),
+                normal_delete(score, record_timestamp),
                 discuss,
             )
         if announce_time >= record_timestamp:
